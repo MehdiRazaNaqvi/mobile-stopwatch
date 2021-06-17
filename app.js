052f8f84ace169cca4f460b318b707188e55c47e
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2021-06-17 09:39:58 -0700

    for mobile view specifically

---
+++ app.js
@@ -0,0 +1,54 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var interval;
+
+
+
+function start() {
+    msec++
+    msecHeading.innerHTML = msec;
+    if(msec >= 05) {
+        sec++
+        secHeading.innerHTML = sec;
+        msec = 0;
+        }
+    if (sec >= 05) {
+        min++
+        minHeading.innerHTML = min;
+        sec = 0;
+          }    
+}
+
+
+function timer() {
+interval = setInterval(start,10);
+document.getElementById("startbtn").disabled = "true"
+}
+
+function pause () { 
+clearInterval(interval);
+document.getElementById('startbtn').innerHTML = "Restart"
+document.getElementById("startbtn").disabled = 0;
+
+}
+
+function reset() {
+    min = 00;
+    sec = 00;
+    msec = 00;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    pause();
+    document.getElementById('startbtn').innerHTML = "Start"
+    
+
+
+
+}
